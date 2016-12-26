dbb40a3b387027604535e318fbe581221bac956f
Brian Kelly <polymonic@gmail.com>
2016-12-26 16:20:10 -0500

    Improve docs a little and bump version

--- lib/piglatin.ex
+++ lib/piglatin.ex
@@ -1,4 +1,19 @@
 defmodule PigLatin do
+  @moduledoc """
+  This is the main PigLatin module for translating words.
+  """
+
+  @doc """
+  Translates the given word to Piglatin.
+
+  Returns the translated word.
+
+  ## Examples
+
+      iex> PigLatin.translate("apple")
+      "appleway"
+
+  """
   def translate(word) do
     if starts_with_vowel(word) do
         word <> "way"
@@ -9,6 +24,17 @@ defmodule PigLatin do
     end
   end
 
+  @doc """
+  Determines if a word starts with a vowel.
+
+  Returns true if the word starts with a vowel. Otherwise false.
+
+  ## Examples
+
+      iex> PigLatin.starts_with_vowel("apple")
+      true
+
+  """
   def starts_with_vowel(word) do
     first_letter = String.downcase(String.first(word))
     Enum.member?(["a", "e", "i", "o", "u"], first_letter)
