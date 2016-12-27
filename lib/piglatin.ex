ccb8909295265a4e19e11b734c24559ecb3c2112
Brian Kelly <polymonic@gmail.com>
2016-12-26 20:58:28 -0500

    Handle words starting with multiple consonants

--- lib/piglatin.ex
+++ lib/piglatin.ex
@@ -18,9 +18,11 @@ defmodule PigLatin do
     if starts_with_vowel(word) do
         word <> "way"
     else
-        first_letter = String.downcase(String.first(word))
-        rest_of_word = String.slice(word, 1..-1)
-        rest_of_word <> first_letter <> "ay"
+        first_vowel_index = get_first_vowel_index(word)
+        first_part = String.slice(word, 0..first_vowel_index-1)
+        rest_of_word = String.slice(word, first_vowel_index..-1)
+
+        rest_of_word <> first_part <> "ay"
     end
   end
 
@@ -38,5 +40,26 @@ defmodule PigLatin do
   def starts_with_vowel(word) do
     first_letter = String.downcase(String.first(word))
     Enum.member?(["a", "e", "i", "o", "u"], first_letter)
+  end
+
+  @doc """
+  Finds the index of the first vowel in a weord.
+
+  Return the index of the first vowel or nil if none found.
+
+  ## Examples
+
+      iex> PigLatin.get_first_vowel_index("apple")
+      0
+  """
+
+  def get_first_vowel_index(word) do
+    Enum.find_index(
+        String.graphemes(word),
+        fn(character) -> Enum.member?(
+            ["a", "e", "i", "o", "u"],
+            character
+        ) end
+    )
   end
 end
--- test/piglatin_test.exs
+++ test/piglatin_test.exs
@@ -11,4 +11,13 @@ defmodule PigLatinTest do
     assert PigLatin.translate("computer") == "omputercay"
     assert PigLatin.translate("coding") == "odingcay"
   end
+
+  test "word starting with multiple consonants" do
+    assert PigLatin.translate("programming") == "ogrammingpray"
+  end
+
+  test "find first vowel" do
+    assert PigLatin.get_first_vowel_index("apple") == 0
+    assert PigLatin.get_first_vowel_index("computer") == 1
+  end
 end
